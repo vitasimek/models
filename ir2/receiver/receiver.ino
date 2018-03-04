0f38a29988c13af8abc2d9fb67f443bac192d28c
vitasi <vita.simek@gmail.com>
2018-03-04 12:45:30 +0100

    IR sender and receiver prototypes.

--- ir2/receiver/receiver.ino
+++ ir2/receiver/receiver.ino
@@ -19,7 +19,8 @@ void setup() {
   Serial.begin(115200);  
 }
 
-volatile bool beep = false;
+volatile int hit_count = 0;
+volatile int last_hit_count = 0;
 
 void loop() {
 
@@ -28,36 +29,43 @@ void loop() {
   int button_2 = digitalRead(BUTTON_2);
   int button_3 = digitalRead(BUTTON_3);
 
-  Serial.print("\tA: ");
-  Serial.print(button_1);
-  Serial.print("\tB: ");
-  Serial.print(button_2);
-  Serial.print("\tC: ");
-  Serial.print(button_3);
-  Serial.print("\tValue: ");
-  Serial.print(value);
-  Serial.print("\tbeep: ");
-  Serial.println(beep);
-
-  if (beep)
+  int hit_count_difference = 0;
+  
+  if (hit_count > last_hit_count)
   {
-    digitalWrite(SPEAKER, HIGH);
-//    tone(SPEAKER, 1000);
+    hit_count_difference = hit_count - last_hit_count;
+
+    int tone_fequency = hit_count_difference * 100;
+    
+
+    tone(SPEAKER, tone_fequency);
   }
   else
   {
-    digitalWrite(SPEAKER, LOW);
-//    noTone(SPEAKER);
+    noTone(SPEAKER);
   }
 
-  digitalWrite(LED, beep);
+  last_hit_count = hit_count;
 
+//  Serial.print("\tA: ");
+//  Serial.print(button_1);
+//  Serial.print("\tB: ");
+//  Serial.print(button_2);
+//  Serial.print("\tC: ");
+//  Serial.print(button_3);
+//  Serial.print("\tValue: ");
+//  Serial.print(value);
+//  Serial.print("\thit_count: ");
+//  Serial.println(hit_count_difference);
 
-  //delay(125);  
+  delay(125);  
 }
 
 void blink()
 {
-  beep = digitalRead(CONTROL);
+  if (digitalRead(CONTROL) == HIGH)
+  {
+    hit_count++;
+  }
 }
 
