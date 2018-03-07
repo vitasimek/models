20f67454638acbc3e2991d3fa1e9bc5723cb1888
Vítězslav Šimek <vita.simek@gmail.com>
2018-03-07 22:13:23 +0100

    Just dummy debug receiver.

--- ir2/receiver/receiver.ino
+++ ir2/receiver/receiver.ino
@@ -1,3 +1,8 @@
+#include <boarddefs.h>
+#include <IRremote.h>
+#include <IRremoteInt.h>
+#include <ir_Lego_PF_BitStreamEncoder.h>
+
 
 #define CONTROL 2
 #define BUTTON_1 5
@@ -6,82 +11,192 @@
 #define LED 13
 #define SPEAKER 6
 
+IRrecv irrecv(CONTROL, LED);
 
 void setup() {
 
   pinMode(LED, OUTPUT);
-  pinMode(CONTROL, INPUT_PULLUP);
+  //pinMode(CONTROL, INPUT_PULLUP);
   pinMode(BUTTON_1, INPUT_PULLUP);
   pinMode(BUTTON_2, INPUT_PULLUP);
   pinMode(BUTTON_3, INPUT_PULLUP);
 
-  attachInterrupt(digitalPinToInterrupt(CONTROL), blink, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(BUTTON_3), set_volume, FALLING);
+  irrecv.blink13(true);
+  irrecv.enableIRIn();
   
   Serial.begin(115200);  
 }
+//
+//void loop() {
+//
+////  int value = digitalRead(CONTROL);
+////  int button_1 = digitalRead(BUTTON_1);
+////  int button_2 = digitalRead(BUTTON_2);
+////  int button_3 = digitalRead(BUTTON_3);
+//
+//  decode_results result;
+//
+//  
+//
+//  Serial.print("\tA: ");
+//}
 
-volatile int hit_count = 0;
-volatile int last_hit_count = 0;
-volatile bool mute = false;
-volatile long volume_before;
 
-void loop() {
 
-  int value = 0;// digitalRead(CONTROL);
-  int button_1 = digitalRead(BUTTON_1);
-  int button_2 = digitalRead(BUTTON_2);
-//  int button_3 = digitalRead(BUTTON_3);
 
-  int hit_count_difference = 0;
-  
-  if (hit_count > last_hit_count)
-  {
-    hit_count_difference = hit_count - last_hit_count;
 
-    int tone_fequency = hit_count_difference * 100;
-    
-    if (!mute)
-    {
-      tone(SPEAKER, tone_fequency);
+
+//+=============================================================================
+// Display IR code
+//
+void  ircode (decode_results *results)
+{
+  // Panasonic has an Address
+  if (results->decode_type == PANASONIC) {
+    Serial.print(results->address, HEX);
+    Serial.print(":");
+  }
+
+  // Print Code
+  Serial.print(results->value, HEX);
+}
+
+//+=============================================================================
+// Display encoding type
+//
+void  encoding (decode_results *results)
+{
+  switch (results->decode_type) {
+    default:
+    case UNKNOWN:      Serial.print("UNKNOWN");       break ;
+    case NEC:          Serial.print("NEC");           break ;
+    case SONY:         Serial.print("SONY");          break ;
+    case RC5:          Serial.print("RC5");           break ;
+    case RC6:          Serial.print("RC6");           break ;
+    case DISH:         Serial.print("DISH");          break ;
+    case SHARP:        Serial.print("SHARP");         break ;
+    case JVC:          Serial.print("JVC");           break ;
+    case SANYO:        Serial.print("SANYO");         break ;
+    case MITSUBISHI:   Serial.print("MITSUBISHI");    break ;
+    case SAMSUNG:      Serial.print("SAMSUNG");       break ;
+    case LG:           Serial.print("LG");            break ;
+    case WHYNTER:      Serial.print("WHYNTER");       break ;
+    case AIWA_RC_T501: Serial.print("AIWA_RC_T501");  break ;
+    case PANASONIC:    Serial.print("PANASONIC");     break ;
+    case DENON:        Serial.print("Denon");         break ;
+  }
+}
+
+//+=============================================================================
+// Dump out the decode_results structure.
+//
+void  dumpInfo (decode_results *results)
+{
+  // Check if the buffer overflowed
+  if (results->overflow) {
+    Serial.println("IR code too long. Edit IRremoteInt.h and increase RAWBUF");
+    return;
+  }
+
+  // Show Encoding standard
+  Serial.print("Encoding  : ");
+  encoding(results);
+  Serial.println("");
+
+  // Show Code & length
+  Serial.print("Code      : ");
+  ircode(results);
+  Serial.print(" (");
+  Serial.print(results->bits, DEC);
+  Serial.println(" bits)");
+}
+
+//+=============================================================================
+// Dump out the decode_results structure.
+//
+void  dumpRaw (decode_results *results)
+{
+  // Print Raw data
+  Serial.print("Timing[");
+  Serial.print(results->rawlen-1, DEC);
+  Serial.println("]: ");
+
+  for (int i = 1;  i < results->rawlen;  i++) {
+    unsigned long  x = results->rawbuf[i] * USECPERTICK;
+    if (!(i & 1)) {  // even
+      Serial.print("-");
+      if (x < 1000)  Serial.print(" ") ;
+      if (x < 100)   Serial.print(" ") ;
+      Serial.print(x, DEC);
+    } else {  // odd
+      Serial.print("     ");
+      Serial.print("+");
+      if (x < 1000)  Serial.print(" ") ;
+      if (x < 100)   Serial.print(" ") ;
+      Serial.print(x, DEC);
+      if (i < results->rawlen-1) Serial.print(", "); //',' not needed for last one
     }
+    if (!(i % 8))  Serial.println("");
   }
-  else
-  {
-    noTone(SPEAKER);
-  }
-
-  last_hit_count = hit_count;
-
-  Serial.print("\tA: ");
-  Serial.print(button_1);
-  Serial.print("\tB: ");
-  Serial.print(button_2);
-//  Serial.print("\tC: ");
-//  Serial.print(button_3);
-  Serial.print("\tValue: ");
-  Serial.print(value);
-  Serial.print("\tmute: ");
-  Serial.println(mute);
-
-  delay(125);  
+  Serial.println("");                    // Newline
 }
 
-void blink()
+//+=============================================================================
+// Dump out the decode_results structure.
+//
+void  dumpCode (decode_results *results)
 {
-  if (digitalRead(CONTROL) == HIGH)
-  {
-    hit_count++;
+  // Start declaration
+  Serial.print("unsigned int  ");          // variable type
+  Serial.print("rawData[");                // array name
+  Serial.print(results->rawlen - 1, DEC);  // array size
+  Serial.print("] = {");                   // Start declaration
+
+  // Dump data
+  for (int i = 1;  i < results->rawlen;  i++) {
+    Serial.print(results->rawbuf[i] * USECPERTICK, DEC);
+    if ( i < results->rawlen-1 ) Serial.print(","); // ',' not needed on last one
+    if (!(i & 1))  Serial.print(" ");
+  }
+
+  // End declaration
+  Serial.print("};");  // 
+
+  // Comment
+  Serial.print("  // ");
+  encoding(results);
+  Serial.print(" ");
+  ircode(results);
+
+  // Newline
+  Serial.println("");
+
+  // Now dump "known" codes
+  if (results->decode_type != UNKNOWN) {
+
+    // Some protocols have an address
+    if (results->decode_type == PANASONIC) {
+      Serial.print("unsigned int  addr = 0x");
+      Serial.print(results->address, HEX);
+      Serial.println(";");
+    }
+
+    // All protocols have data
+    Serial.print("unsigned int  data = 0x");
+    Serial.print(results->value, HEX);
+    Serial.println(";");
   }
 }
 
-void set_volume()
+void  loop ( )
 {
-  long volume_now = millis();
-  if (volume_now - volume_before >= 50)
-  {
-    mute = !mute; 
-    volume_before = millis();
+  decode_results  results;        // Somewhere to store the results
+
+  if (irrecv.decode(&results)) {  // Grab an IR code
+    dumpInfo(&results);           // Output the results
+    dumpRaw(&results);            // Output the results in RAW format
+    dumpCode(&results);           // Output the results as source code
+    Serial.println("");           // Blank line between entries
+    irrecv.resume();              // Prepare for the next value
   }
 }
-
